6f9f337fd92b3861068cee6bf6291063fab643a8
Constanza Morales A <61435709+conimorales@users.noreply.github.com>
2023-03-12 19:54:02 -0300

    productos 3

--- 2.constructor.js
+++ 2.constructor.js
@@ -1,1 +1,21 @@
-import fs from 'fs'
+import ManagerUsuarios from "./ManagerUsuarios.js"
+
+const manager = new ManagerUsuarios();
+
+const env = async() =>{
+    let primeraConsultaUs= await manager.consultarUsuarios();
+
+    let user = {
+        nombre: "Mauricio",
+        apellido: "Espinoza",
+        edad:26,
+        curso: "Backend"
+    }
+
+    let result = await manage.crearUsuario(user);
+    console.log(result)
+    let segundaConsultaUs = await manager.consultarUsuarios()
+    console.log(segundaConsultaUs)
+}
+
+env();
